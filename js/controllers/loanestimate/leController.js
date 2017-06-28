d22d38c2eaf01f953cdb870122dd027b16182fa1
Pugaz P <pugaz.p@compugain.com>
2017-06-28 18:02:23 +0530

    Help classes added

--- js/controllers/loanestimate/leController.js
+++ js/controllers/loanestimate/leController.js
@@ -9,6 +9,7 @@ app.controller('loanEstimateCtrl', function ($scope, $sce,$rootScope, $filter,$l
 	if(localStorage.loanFormType != undefined) {
 		staticData.basicLoanInfo.loanFormType = localStorage.loanFormType;
 	}
+	$(".helpIcon").tooltip();
 	$scope.rateLokedTime=["1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00",
 	"9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00",
 	"20:00","21:00","22:00","23:00","24:00"];
