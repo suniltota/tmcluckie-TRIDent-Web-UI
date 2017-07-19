6054d600171b102a93755bbe486133b6485512dc
Bhushan Kollepara <bhushan.kollepara@compugain.com>
2017-07-19 19:51:46 +0530

    TTK-40 CD & LE(Fixed)- Principal and Interest Amount in Loan Terms

--- js/controllers/loanestimate/leController.js
+++ js/controllers/loanestimate/leController.js
@@ -1105,6 +1105,44 @@ app.controller('loanEstimateCtrl', function ($scope, $sce,$rootScope, $filter,$l
     	$scope.leformdata.payment.paymentRule.seasonalPaymentPeriodEndMonth = '';
     }
     
+    $scope.stepChange = function(val){
+    	if(val==true){
+    		$scope.stepPaymentIndicator = true;
+    	}else{
+    		$scope.stepPaymentIndicator = false;
+    	}
+		$scope.leformdata.interestRateAdjustment.totalStepCount = '';
+    	$scope.leformdata.payment.paymentRule.totalStepPaymentCount = '';
+    }
+
+    $scope.optionalChange = function(){
+    	$scope.leformdata.payment.paymentRule.totalOptionalPaymentCount = '';
+    }
+
+    $scope.temporaryChange = function(){
+    	$scope.leformdata.loanTerms.temporaryBuydown.gseBuydownReflectedInNoteIndicator = null;
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownInitialEffectiveInterestRatePercent = '';
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownChangeFrequencyMonthsCount = '';
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownDurationMonthsCount = '';
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownIncreaseRatePercent = '';
+    	$scope.leformdata.termsOfLoan.noteRatePercent = '';
+    	$scope.leformdata.termsOfLoan.disclosedFullyIndexedRatePercent = '';
+    }
+    $scope.temporaryBuydownChange = function(){
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownInitialEffectiveInterestRatePercent = '';
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownChangeFrequencyMonthsCount = '';
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownDurationMonthsCount = '';
+    	$scope.leformdata.loanTerms.temporaryBuydown.buydownIncreaseRatePercent = '';
+    }
+
+    $scope.principalAndInterestChange = function(){
+       $scope.leformdata.principalAndInterestPaymentAdjustment.firstPerChangePrincipalAndInterestPaymentAdjustmentFrequencyMonthsCount = '';
+       $scope.leformdata.principalAndInterestPaymentAdjustment.firstPrincipalAndInterestPaymentChangeMonthsCount = '';
+       $scope.leformdata.principalAndInterestPaymentAdjustment.principalAndInterestPaymentMaximumAmount = '';
+       $scope.leformdata.principalAndInterestPaymentAdjustment.principalAndInterestPaymentMaximumAmountEarliestEffectiveMonthsCount = '';
+       $scope.leformdata.principalAndInterestPaymentAdjustment.principalAndInterestPaymentMaximumAmountEarliestEffectiveYearCount = '';
+    }
+    
     $scope.prepaymentPenaltyChange = function(){
     	$scope.leformdata.loanTerms.prepaymentPenalty.prepaymentPenaltyMaximumLifeOfLoanAmount = '';
     	$scope.leformdata.loanTerms.prepaymentPenalty.prepaymentPenaltyExpirationMonthsCount = '';
