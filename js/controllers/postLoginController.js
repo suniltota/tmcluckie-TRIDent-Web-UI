cad2502dd1456c78b7739a622a01bf59e488ca95
Bhushan Kollepara <bhushan.kollepara@compugain.com>
2017-07-19 12:38:52 +0530

    Added Errors Handlling functionality while uploading invalid Template Files

--- js/controllers/postLoginController.js
+++ js/controllers/postLoginController.js
@@ -109,7 +109,7 @@ postLoginApp.controller('postLoginCtrl', function ($scope, $window, loginService
                         console.log(localStorage.jsonData);
                         location.href = "index.html#/home?documentType="+$scope.documentType+"&purposeType="+$scope.purposeType+"&formType="+$scope.formType;
                     }).error(function(data, status) {
-                        $scope.fileerror = 'There is a problem with xml file. Please select valid xml file';
+                        $scope.fileerror = 'There is a problem with template file. Please select valid template file';
                         $("#spinner").hide();
                     });
                 } else {
@@ -129,7 +129,7 @@ postLoginApp.controller('postLoginCtrl', function ($scope, $window, loginService
                         localStorage.jsonData = JSON.stringify(jsondata);
                         location.href = "index.html#/home?documentType="+$scope.documentType+"&purposeType="+$scope.purposeType+"&formType="+$scope.formType;
                     }).error(function(data, status) {
-                        $scope.fileerror = 'There is a problem with xml file. Please select valid xml file';
+                        $scope.fileerror = 'There is a problem with template file. Please select valid template file';
                         $("#spinner").hide();
                     });
                 }
