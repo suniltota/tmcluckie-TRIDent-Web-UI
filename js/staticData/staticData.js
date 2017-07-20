c6e2a7a0403b1fb11566f1fa10250071903a7996
Shravan Kumar Boragala <shravan.boragala@compugain.com>
2017-07-20 17:31:49 +0530

    changed months for payment rule as per ucd spec

--- js/staticData/staticData.js
+++ js/staticData/staticData.js
@@ -2171,7 +2171,56 @@ app.service('staticData', function($filter){
 		}
 	];
 
-	this.calendarMonths = ['Jan','Feb','Mar', 'Apr', 'May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	this.calendarMonths = [
+		{
+			'name' : 'Jan',
+			'value': '--01'
+		},
+		{
+			'name':'Feb',
+			'value':'--02'
+		},
+		{
+			'name':'Mar',
+			'value':'--03'
+		},
+		{
+			'name':'Apr',
+			'value':'--04'
+		},
+		{
+			'name':'May',
+			'value':'--05'
+		},
+		{
+			'name':'Jun',
+			'value':'--06'
+		},
+		{
+			'name':'Jul',
+			'value':'--07'
+		},
+		{
+			'name':'Aug',
+			'value':'--08'
+		},
+		{
+			'name':'Sep',
+			'value':'--09'
+		},
+		{
+			'name':'Oct',
+			'value':'--10'
+		},
+		{
+			'name':'Nov',
+			'value':'--11'
+		},
+		{
+			'name':'Dec',
+			'value':'--12'
+		}
+	];
 
     this.prepaidItems = [
 		{
