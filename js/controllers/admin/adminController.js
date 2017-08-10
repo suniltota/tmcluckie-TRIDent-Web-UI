598ed85bb81e14df45c1bb3fa99cc3c57ef60a36
Sivaji Ganga <sganga@csilhyd1.com>
2017-08-10 13:07:06 +0530

    add client in admin page

--- js/controllers/admin/adminController.js
+++ js/controllers/admin/adminController.js
@@ -46,4 +46,75 @@ app.controller('adminCtrl', function ($scope) {
         {"keycolumn1":14,"originkey1":14,"datafield1":14},
         {"keycolumn1":15,"originkey1":15,"datafield1":15}
     ];
-});
+
+
+    
+		$scope.listbox_move = function(listID, direction) {
+
+			var listbox = document.getElementById(listID);
+			var selIndex = listbox.selectedIndex;
+
+			if(-1 == selIndex) {
+				alert("Please select an option to move.");
+				return;
+			}
+
+			var increment = -1;
+			if(direction == 'up')
+				increment = -1;
+			else
+				increment = 1;
+
+			if((selIndex + increment) < 0 ||
+				(selIndex + increment) > (listbox.options.length-1)) {
+				return;
+			}
+
+			var selValue = listbox.options[selIndex].value;
+			var selText = listbox.options[selIndex].text;
+			listbox.options[selIndex].value = listbox.options[selIndex + increment].value
+			listbox.options[selIndex].text = listbox.options[selIndex + increment].text
+
+			listbox.options[selIndex + increment].value = selValue;
+			listbox.options[selIndex + increment].text = selText;
+
+			listbox.selectedIndex = selIndex + increment;
+		}
+
+		$scope.listbox_moveacross = function(sourceID, destID) {
+			var src = document.getElementById(sourceID);
+			var dest = document.getElementById(destID);
+
+			for(var count=0; count < src.options.length; count++) {
+
+				if(src.options[count].selected == true) {
+						var option = src.options[count];
+
+						var newOption = document.createElement("option");
+						newOption.value = option.value;
+						newOption.text = option.text;
+						newOption.selected = true;
+						try {
+								 dest.add(newOption, null); //Standard
+								 src.remove(count, null);
+						 }catch(error) {
+								 dest.add(newOption); // IE only
+								 src.remove(count);
+						 }
+						count--;
+
+				}
+
+			}
+
+		}
+		$scope.listbox_selectall = function(listID, isSelect) {
+
+			var listbox = document.getElementById(listID);
+			for(var count=0; count < listbox.options.length; count++) {
+
+				listbox.options[count].selected = isSelect;
+
+			}
+		}
+	});
