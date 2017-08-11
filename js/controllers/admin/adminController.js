c8b8ac367b7a402b7c8b50ff0b342f89619ed58b
Shaik Mohammed Ali <mohammed.ali@compugain.com>
2017-08-11 08:40:16 +0100

    Group ui seperated and created a new controller

--- js/controllers/admin/adminController.js
+++ js/controllers/admin/adminController.js
@@ -1,4 +1,4 @@
-app.controller('adminCtrl', function ($scope, $window, apiService) {
+app.controller('adminCtrl', function ($rootScope, $scope, $window, apiService) {
 
     $scope.showTab = 'adminDashboard';
     $scope.viewContent = 'viewClient';
@@ -35,31 +35,7 @@ app.controller('adminCtrl', function ($scope, $window, apiService) {
     $scope.AvailablePermissionsName = function( AvailablePermission ) {
         return AvailablePermission.name;
     };
-    //For Group
-    $scope.availableGroupPermissions = [{
-            serviceId: "a041cfee-7c22-11e7-bb31-be2e44b06b34",
-            serviceName: "JSONTOCDPDF",
-            serviceDisplayName: "JSON to CD PDF"        
-        },{
-            serviceId: "a041d11a-7c22-11e7-bb31-be2e44b06b34",
-            serviceName: "JSONTOLEPDF",
-            serviceDisplayName: "JSON to LE PDF"        
-        },{
-            serviceId: "a041d2d2-7c22-11e7-bb31-be2e44b06b34",
-            serviceName: "JSONTOCDJSONWithCalculations",
-            serviceDisplayName: "JSON to CD JSON with Calculations"        
-        }];
-    $scope.availableGroupNmaes = $scope.availableGroupPermissions;
-    $scope.availableGroupPermissionsName = function( availableGroupPermissions ) {
-        return availableGroupPermissions.serviceDisplayName;
-    };
-
-    $scope.GrantedGroupPermissions = [];
-    $scope.GrantedGroupNmaes = $scope.GrantedGroupPermissions;
-    $scope.GrantedGroupPermissionsName = function( GrantedGroupPermission ) {
-        return GrantedGroupPermission.serviceDisplayName;
-    };
-
+    
     $scope.newAdminTab = function(content){
         //location.href = "index.html#/admin";
         $scope.viewContent = content;//'addClient';
@@ -71,7 +47,8 @@ app.controller('adminCtrl', function ($scope, $window, apiService) {
         }
         if(content == 'viewGroup' && $scope.groupList && !$scope.groupList.length){
             $("#spinner").show();
-            $scope.getGroupData();
+            $rootScope.$emit("loadGroupData", {});
+            //$scope.getGroupData();
         }else if(content == 'addGroup'){
             $scope.addEditGroup = {};
         }else if(content == 'viewUser'){
@@ -83,88 +60,6 @@ app.controller('adminCtrl', function ($scope, $window, apiService) {
     }
 
    
-    $scope.getGroupData = function() {
-        apiService.request({apiMethod:'actualize/transformx/groups',httpMethod:'GET'}).success(function(data, status) {
-            $scope.groupList = data;
-            $scope.parentGroupListUnderAdmin = [];
-            for(var i=0;i<data.length;i++){
-                $scope.parentGroupListUnderAdmin.push({"groupId":data[i].groupId, "groupName":data[i].groupName});
-            }
-            $scope.prntGrp = $scope.parentGroupListUnderAdmin[0];
-            $("#spinner").hide();
-        }).error(function(data, status) {
-            $("#spinner").hide();
-            console.log("API  'actualize/transformx/groups' Error: "+data);
-        });
-    }
-$scope.parentGroupChange = function(selectedGroup){
-    $scope.prntGrp = selectedGroup;
-}
-    $scope.saveGroup = function() {
-        if(!$scope.addEditGroup.length){
-              $scope.groupDetails = {"groupId":($scope.addEditGroup.groupId)?$scope.addEditGroup.groupId:"","groupName":$scope.addEditGroup.groupName,"groupParentId":$scope.prntGrp.groupId,"sessionTimeOut":$scope.sessTOut.value,"passwordExpireDays":$scope.pwdExp.value,"services":$scope.availableGroupPermissions,"clientId"  : "a00deb64-7832-11e7-b5a5-be2e44b06b34"};
-                $("#spinner").show();
-            if($scope.addEditGroup.groupId){
-                apiService.request({apiMethod:'actualize/transformx/groups',formData:$scope.groupDetails,httpMethod:'PUT'}).success(function(data, status) {
-                    $window.alert(data);
-                    $("#spinner").hide();
-                    $scope.getGroupData();
-                    $scope.newAdminTab('viewGroup');
-                }).
-                error(function(data, status) {
-                    console.log("API actualize/transformx/groups : "+data);
-                });
-            }else{
-                apiService.request({apiMethod:'actualize/transformx/groups',formData:$scope.groupDetails,httpMethod:'POST'}).success(function(data, status) {
-                    $window.alert(data);
-                    $("#spinner").hide();
-                    $scope.getGroupData();
-                    $scope.newAdminTab('viewGroup');
-                }).
-                error(function(data, status) {
-                    console.log("API actualize/transformx/groups : "+data);
-                });
-            }
-        }
-    }
-
-    $scope.editGroup = function(groupId) {
-            $("#spinner").show();
-            $scope.newAdminTab('addGroup');
-        apiService.request({apiMethod:'actualize/transformx/groups/'+groupId,httpMethod:'GET'}).success(function(data, status) {
-            $scope.addEditGroup = data;
-            $("#spinner").hide();
-        }).error(function(data, status) {
-            $("#spinner").hide();
-            console.log("API  editGroup Error: "+data);
-        });
-    }
-
-    $scope.updateGroup = function(groupId) {
-            $("#spinner").show();
-            $scope.newAdminTab('addGroup');
-        apiService.request({apiMethod:'actualize/transformx/groups/'+groupId,httpMethod:'GET'}).success(function(data, status) {
-            $scope.addEditGroup = data;
-            $("#spinner").hide();
-        }).error(function(data, status) {
-            $("#spinner").hide();
-            console.log("API  editGroup Error: "+data);
-        });
-    }
-
-    $scope.deleteGroup = function(groupId) {
-            $("#spinner").show();
-        apiService.request({apiMethod:'actualize/transformx/groups/'+groupId,httpMethod:'DELETE'}).success(function(data, status) {
-            console.log("API actualize/transformx/groups : "+data);
-            //$scope.groupList = data;
-            $window.alert(data);
-            $("#spinner").hide();
-        }).error(function(data, status) {
-            $("#spinner").hide();
-            console.log("API  'actualize/transformx/groups' Error: "+data);
-        });
-    }
-
 
         $scope.listbox_move = function(listID, direction) {
 
@@ -390,4 +285,127 @@ app.directive('ngConfirmClick', [
                     });
                 }
             };
-    }])
+    }]);
+
+//Below controller is for Group tab
+app.controller('groupCtrl', function ($rootScope, $scope, $window, apiService) {
+    $scope.viewContent='viewGroup';
+    //For Group
+    $scope.availableGroupPermissions = [{
+            serviceId: "a041cfee-7c22-11e7-bb31-be2e44b06b34",
+            serviceName: "JSONTOCDPDF",
+            serviceDisplayName: "JSON to CD PDF"        
+        },{
+            serviceId: "a041d11a-7c22-11e7-bb31-be2e44b06b34",
+            serviceName: "JSONTOLEPDF",
+            serviceDisplayName: "JSON to LE PDF"        
+        },{
+            serviceId: "a041d2d2-7c22-11e7-bb31-be2e44b06b34",
+            serviceName: "JSONTOCDJSONWithCalculations",
+            serviceDisplayName: "JSON to CD JSON with Calculations"        
+        }];
+    $scope.availableGroupNmaes = $scope.availableGroupPermissions;
+    $scope.availableGroupPermissionsName = function( availableGroupPermissions ) {
+        return availableGroupPermissions.serviceDisplayName;
+    };
+
+    $scope.GrantedGroupPermissions = [];
+    $scope.GrantedGroupNmaes = $scope.GrantedGroupPermissions;
+    $scope.GrantedGroupPermissionsName = function( GrantedGroupPermission ) {
+        return GrantedGroupPermission.serviceDisplayName;
+    };
+     $scope.newAdminTab = function(content){
+        $scope.viewContent = content;//'addClient';        
+        if(content == 'viewGroup' && $scope.groupList && !$scope.groupList.length){
+            $("#spinner").show();
+            $scope.getGroupData();
+        }else if(content == 'addGroup'){
+            $scope.addEditGroup = {};
+        }
+    }
+$rootScope.$on("loadGroupData", function(){
+           $scope.getGroupData();
+        });
+    $scope.getGroupData = function() {
+        apiService.request({apiMethod:'actualize/transformx/groups',httpMethod:'GET'}).success(function(data, status) {
+            $scope.groupList = data;
+            $scope.parentGroupListUnderAdmin = [];
+            for(var i=0;i<data.length;i++){
+                $scope.parentGroupListUnderAdmin.push({"groupId":data[i].groupId, "groupName":data[i].groupName});
+            }
+            $scope.prntGrp = $scope.parentGroupListUnderAdmin[0];
+            $("#spinner").hide();
+        }).error(function(data, status) {
+            $("#spinner").hide();
+            console.log("API  'actualize/transformx/groups' Error: "+data);
+        });
+    }
+$scope.parentGroupChange = function(selectedGroup){
+    $scope.prntGrp = selectedGroup;
+}
+    $scope.saveGroup = function() {
+        if(!$scope.addEditGroup.length){
+              $scope.groupDetails = {"groupId":($scope.addEditGroup.groupId)?$scope.addEditGroup.groupId:"","groupName":$scope.addEditGroup.groupName,"groupParentId":$scope.prntGrp.groupId,"sessionTimeOut":$scope.sessTOut.value,"passwordExpireDays":$scope.pwdExp.value,"services":$scope.availableGroupPermissions,"clientId"  : "a00deb64-7832-11e7-b5a5-be2e44b06b34"};
+                $("#spinner").show();
+            if($scope.addEditGroup.groupId){
+                apiService.request({apiMethod:'actualize/transformx/groups',formData:$scope.groupDetails,httpMethod:'PUT'}).success(function(data, status) {
+                    $window.alert(data);
+                    $("#spinner").hide();
+                    $scope.getGroupData();
+                    $scope.newAdminTab('viewGroup');
+                }).
+                error(function(data, status) {
+                    console.log("API actualize/transformx/groups : "+data);
+                });
+            }else{
+                apiService.request({apiMethod:'actualize/transformx/groups',formData:$scope.groupDetails,httpMethod:'POST'}).success(function(data, status) {
+                    $window.alert(data);
+                    $("#spinner").hide();
+                    $scope.getGroupData();
+                    $scope.newAdminTab('viewGroup');
+                }).
+                error(function(data, status) {
+                    console.log("API actualize/transformx/groups : "+data);
+                });
+            }
+        }
+    }
+
+    $scope.editGroup = function(groupId) {
+            $("#spinner").show();
+            $scope.newAdminTab('addGroup');
+        apiService.request({apiMethod:'actualize/transformx/groups/'+groupId,httpMethod:'GET'}).success(function(data, status) {
+            $scope.addEditGroup = data;
+            $("#spinner").hide();
+        }).error(function(data, status) {
+            $("#spinner").hide();
+            console.log("API  editGroup Error: "+data);
+        });
+    }
+
+    $scope.updateGroup = function(groupId) {
+            $("#spinner").show();
+            $scope.newAdminTab('addGroup');
+        apiService.request({apiMethod:'actualize/transformx/groups/'+groupId,httpMethod:'GET'}).success(function(data, status) {
+            $scope.addEditGroup = data;
+            $("#spinner").hide();
+        }).error(function(data, status) {
+            $("#spinner").hide();
+            console.log("API  editGroup Error: "+data);
+        });
+    }
+
+    $scope.deleteGroup = function(groupId) {
+            $("#spinner").show();
+        apiService.request({apiMethod:'actualize/transformx/groups/'+groupId,httpMethod:'DELETE'}).success(function(data, status) {
+            console.log("API actualize/transformx/groups : "+data);
+            //$scope.groupList = data;
+            $window.alert(data);
+            $("#spinner").hide();
+        }).error(function(data, status) {
+            $("#spinner").hide();
+            console.log("API  'actualize/transformx/groups' Error: "+data);
+        });
+    }
+
+});
